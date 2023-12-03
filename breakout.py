5bcd184657b173bb3898e2a964db8ed0aeafa24e
Matheus_Takashi_Maruoka_Vieira <mtmv.snf23@uea.edu.br>
2023-12-03 14:57:51 -0300

    Restart mechanic add and Fixed player collision

--- breakout.py
+++ breakout.py
@@ -47,12 +47,12 @@ player_lifes = 3
 bounce_sound_effect = pygame.mixer.Sound('assets/bounce.wav')
 
 # creating player 1
-player_speed = screen_size_x * 0.010
+player_speed = screen_size_x * 0.00769
 player_1_size_y = screen_size_y * 0.0256
 player_1_size_x = screen_size_x * 0.0769
 player_1_y_spawn = screen_size_y * 0.8974
 player_1_x_spawn = screen_size_x / 2 - (player_1_size_x / 2)
-player_1_position_and_design = pygame.Rect(player_1_x_spawn, player_1_y_spawn, player_1_size_x, player_1_size_y)
+player_1_design_and_position = pygame.Rect(player_1_x_spawn, player_1_y_spawn, player_1_size_x, player_1_size_y)
 player_1_waiting = pygame.Rect(0, player_1_y_spawn, 1000, 20)
 player_1_move_right = False
 player_1_move_left = False
@@ -60,7 +60,7 @@ waiting_for_start = True
 
 # creating ball
 ball_x_size = screen_size_x * 0.0153
-ball_y_size = screen_size_y * 0.0153
+ball_y_size = screen_size_y * 0.0128
 ball_x_spawn = (screen_size_x/2) - ball_x_size
 ball_y_spawn = (screen_size_y * 0.4871) + ball_y_size
 ball_x_speed = screen_size_x * 0.0069
@@ -73,12 +73,13 @@ ball_dy = ball_y_speed
 
 
 # score
-score = 000
-SCORE_MAX = 896
+score = 0
+SCORE_MAX = 448
 
 # game restart
 game_restart = True
-
+waiting_for_restart = True
+game_loop_after_death = True
 # game loop
 game_loop = True
 game_clock = pygame.time.Clock()
@@ -95,9 +96,10 @@ retangle_size_x = screen_size_x * 0.0615
 retangle_size_y = screen_size_y * 0.0141
 
 while game_restart:
-    game_restart = False
     game_loop = True
-    player_design = player_1_position_and_design
+    waiting_for_restart = True
+    player_lifes = 3
+    player_design = player_1_design_and_position
 
     red_obstacles = []
     for n in range(2):
@@ -135,10 +137,10 @@ while game_restart:
                         ball_random_x = random.choice(ball_random_x_list)
                         waiting_for_start = False
                         ball = pygame.Rect(ball_x_spawn, ball_y_spawn, 0, 0)
-                        ball_dy = ball_y_speed
-                        ball_dx = ball_random_x
+                        ball_dy = 0
+                        ball_dx = 0
                 # checking the victory condition in waiting
-            if score < SCORE_MAX:
+            if game_loop:
 
                 # clear screen
                 screen.fill(COLOR_BLACK)
@@ -151,10 +153,10 @@ while game_restart:
                     bounce_sound_effect.play()
 
                 # ball collision with player in waiting
-                ball_in_paddle_range_y = player_1_position_and_design.y + (player_1_size_y / 2) >= ball.y + ball_y_size >= player_1_position_and_design.y
+                ball_in_paddle_range_y = player_1_design_and_position.y + (player_1_size_y / 2) >= ball.y + ball_y_size >= player_1_design_and_position.y
 
                 if ball_in_paddle_range_y and player_1_waiting.x + 1000 >= ball.x >= player_1_waiting.x:
-                    ball.y = player_1_position_and_design.y - ball_y_size
+                    ball.y = player_1_design_and_position.y - ball_y_size
                     ball_dy *= -1
                     bounce_sound_effect.play()
 
@@ -222,6 +224,8 @@ while game_restart:
 
             if event.type == pygame.QUIT:
                 game_loop = False
+                game_restart = False
+                waiting_for_start = False
 
             #  keystroke events
 
@@ -235,23 +239,22 @@ while game_restart:
                     player_1_move_right = False
                 if event.key == pygame.K_LEFT:
                     player_1_move_left = False
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_RCTRL:
-                    ball_spawn = True
-                    if ball_spawn:
-                        ball_random_x = random.choice(ball_random_x_list)
-                        ball = pygame.Rect(ball_x_spawn, ball_y_spawn, ball_x_size, ball_y_size)
-                        ball_dy = ball_y_speed
-                        ball_dx = ball_random_x
-                        pygame.draw.rect(screen, COLOR_WHITE, ball)
+            if player_lifes > 0:
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_RCTRL:
+                        ball_spawn = True
+                        if ball_spawn:
+                            ball_random_x = random.choice(ball_random_x_list)
+                            ball = pygame.Rect(ball_x_spawn, ball_y_spawn, ball_x_size, ball_y_size)
+                            ball_dy = ball_y_speed
+                            ball_dx = ball_random_x
+                            pygame.draw.rect(screen, COLOR_WHITE, ball)
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_F1:
-                    game_restart == True
-                    game_loop == False
                     waiting_for_start == False
 
-        # checking the victory condition
-        if score < SCORE_MAX:
+        # start game loop
+        if game_loop:
 
             # clear screen
             screen.fill(COLOR_BLACK)
@@ -271,26 +274,28 @@ while game_restart:
 
             # ball collision with player in game
 
-            ball_in_paddle_range_y = player_1_position_and_design.y + (player_1_size_y / 2) >= ball.y + ball_y_size >= player_1_position_and_design.y
-            ball_in_side_paddle_range = player_1_position_and_design.y + player_1_size_y >= ball.y + (ball_y_size / 2) >= player_1_position_and_design.y
-            ball_in_right_paddle_side_x = player_1_position_and_design.x + player_1_size_x >= ball.x >= player_1_position_and_design.x + (player_1_size_x / 2)
-            ball_in_left_paddle_side_x = player_1_position_and_design.x + (player_1_size_x / 2) >= ball.x + ball_x_size >= player_1_position_and_design.x
+            ball_in_paddle_range_y = player_1_design_and_position.y <= ball.y + ball_y_size < (player_1_design_and_position.y + player_1_size_y/2)
+            ball_in_side_paddle_range_y = player_1_design_and_position.y < ball.y + (ball_y_size/2) <= player_1_design_and_position.y + player_1_size_y
+            ball_in_top_paddle_x_bottomleft = player_1_design_and_position.x < ball.x < player_1_design_and_position.x + player_1_size_x
+            ball_in_top_paddle_x_bottomright = player_1_design_and_position.x < ball.x + ball_x_size < player_1_design_and_position.x + player_1_size_x
+            ball_in_right_paddle_side_x = player_1_design_and_position.x + (player_1_size_x/2) <= ball.x <= player_1_design_and_position.x + player_1_size_x
+            ball_in_left_paddle_side_x = player_1_design_and_position.x <= ball.x + ball_x_size <= player_1_design_and_position.x + (player_1_size_x/2)
 
-            if ball_in_paddle_range_y and player_1_position_and_design.x + player_1_size_x >= ball.x + (ball_x_size / 2) >= player_1_position_and_design.x:
-                ball.y = player_1_position_and_design.y - ball_y_size
+            if ball_in_paddle_range_y and (ball_in_top_paddle_x_bottomleft or ball_in_top_paddle_x_bottomright):
+                ball.y = player_1_design_and_position.y - (ball_y_size - 3)
                 ball_dy *= -1
                 bounce_sound_effect.play()
 
             # collision with the right side
-            if ball_in_side_paddle_range and ball_in_right_paddle_side_x:
-                ball.x = player_1_position_and_design.x + player_1_size_x + 3
+            if ball_in_side_paddle_range_y and ball_in_right_paddle_side_x:
+                ball.x = player_1_design_and_position.x + player_1_size_x + 3
                 ball_dy *= -1
                 ball_dx *= -1
                 bounce_sound_effect.play()
 
             # collision with the left side
-            if ball_in_side_paddle_range and ball_in_left_paddle_side_x:
-                ball.x = player_1_position_and_design.x - (ball_x_size + 3)
+            if ball_in_side_paddle_range_y and ball_in_left_paddle_side_x:
+                ball.x = player_1_design_and_position.x - (ball_x_size + 3)
                 ball_dy *= -1
                 ball_dx *= -1
                 bounce_sound_effect.play()
@@ -311,23 +316,23 @@ while game_restart:
 
             # player 1 right movement
             if player_1_move_right:
-                player_1_position_and_design.right += player_speed
+                player_1_design_and_position.right += player_speed
             else:
-                player_1_position_and_design.right -= 0
+                player_1_design_and_position.right -= 0
 
             # player 1  movement
             if player_1_move_left:
-                player_1_position_and_design.left -= player_speed
+                player_1_design_and_position.left -= player_speed
             else:
-                player_1_position_and_design.left += 0
+                player_1_design_and_position.left += 0
 
             # player 1 collides with left wall
-            if player_1_position_and_design.left <= 0:
-                player_1_position_and_design.left = 0
+            if player_1_design_and_position.left <= 0:
+                player_1_design_and_position.left = 0
 
             # player 1 collides with right wall
-            elif player_1_position_and_design.right >= screen_size_x:
-                player_1_position_and_design.right = screen_size_x
+            elif player_1_design_and_position.right >= screen_size_x:
+                player_1_design_and_position.right = screen_size_x
 
             # yellow obstacle collision
             collision_yellow = ball.collidelist(yellow_obstacles)
@@ -380,97 +385,39 @@ while game_restart:
             pygame.draw.rect(screen, COLOR_WHITE, ball)
             screen.blit(score_text, score_text_rect)
             screen.blit(player_lifes_text, player_lifes_rect)
-        else:
+        if score >= SCORE_MAX:
+            ball_in_paddle_range_y = player_1_waiting.y + (player_1_size_y / 2) >= ball.y + ball_y_size >= player_1_waiting.y
             player_design = player_1_waiting
+            if ball_in_paddle_range_y and player_1_waiting.x + 1000 >= ball.x >= player_1_waiting.x:
+                ball.y = player_1_design_and_position.y - ball_y_size
+                ball_dy *= -1
+                bounce_sound_effect.play()
 
-        if player_lifes == 0:
-
-            waiting_for_start == True
-
-            while waiting_for_start:
+        if player_lifes <= 0:
+            while waiting_for_restart:
                 for event in pygame.event.get():
                     if event.type == pygame.QUIT:
-                        waiting_for_start = False
+                        waiting_for_restart = False
+                        game_loop_after_death = False
+                        game_restart = False
                         game_loop = False
                     if event.type == pygame.KEYDOWN:
-                        if event.key == pygame.K_SPACE:
+                        if event.key == pygame.K_r:
                             ball_random_x = random.choice(ball_random_x_list)
-                            waiting_for_start = False
+                            game_loop_after_death = False
+                            waiting_for_restart = False
+                            game_loop = False
                             ball = pygame.Rect(ball_x_spawn, ball_y_spawn, 0, 0)
-                            ball_dy = ball_y_speed
-                            ball_dx = ball_random_x
-                    # checking the victory condition in waiting after death
-                if score < SCORE_MAX:
+                            ball_dy = 0
+                            ball_dx = 0
+                            player_1_design_and_position.x = player_1_x_spawn
+                            player_1_design_and_position.y = player_1_y_spawn
 
-                    # clear screen
-                    screen.fill(COLOR_BLACK)
-
-                    # ball collision with top in waiting after death
-
-                    if ball.top <= 0:
-                        ball.y = 1
-                        ball_dy *= -1
-                        bounce_sound_effect.play()
-
-                    # ball collision with player in waiting after death
-                    ball_in_paddle_range_y = player_1_position_and_design.y + (player_1_size_y / 2) >= ball.y + ball_y_size >= player_1_position_and_design.y
-
-                    if ball_in_paddle_range_y and player_1_waiting.x + 1000 >= ball.x >= player_1_waiting.x:
-                        ball.y = player_1_position_and_design.y - ball_y_size
-                        ball_dy *= -1
-                        bounce_sound_effect.play()
-
-                    # ball collision with the wall in waiting after death
-                    if ball.right >= screen_size_x:
-                        ball.x = screen_size_x - (ball_x_size + 1)
-                        ball_dx *= -1
-                        bounce_sound_effect.play()
-                    elif ball.left <= 0:
-                        ball.x = 1
-                        ball_dx *= -1
-                        bounce_sound_effect.play()
-
-                    # ball movement in waiting after death
-                    ball.x += ball_dx
-                    ball.y += ball_dy
-
-                    # yellow obstacle collision in waiting after death
-                    collision_yellow = ball.collidelist(yellow_obstacles)
-                    if collision_yellow != -1:
-                        ball_dy *= -1
-                        bounce_sound_effect.play()
-
-                    # red obstacle collision in waiting after death
-                    collision_red = ball.collidelist(red_obstacles)
-                    if collision_red != -1:
-                        ball_dy *= -1
-                        bounce_sound_effect.play()
-
-                    # green obstacle collision in waiting after death
-                    collision_green = ball.collidelist(green_obstacles)
-                    if collision_green != -1:
-                        ball_dy *= -1
-                        bounce_sound_effect.play()
-
-                    # orange obstacle collision in waiting after death
-                    collision_orange = ball.collidelist(orange_obstacles)
-                    if collision_orange != -1:
-                        ball_dy *= -1
-                        bounce_sound_effect.play()
-
-                    # drawing obstacles in waiting after death
-                    for red_obstacle in red_obstacles:
-                        pygame.draw.rect(screen, COLOR_RED, red_obstacle)
-                    for orange_obstacle in orange_obstacles:
-                        pygame.draw.rect(screen, COLOR_ORANGE, orange_obstacle)
-                    for green_obstacle in green_obstacles:
-                        pygame.draw.rect(screen, COLOR_GREEN, green_obstacle)
-                    for yellow_obstacle in yellow_obstacles:
-                        pygame.draw.rect(screen, COLOR_YELLOW, yellow_obstacle)
+                # looping game death
+                if game_loop_after_death:
 
                     # drawing objects in waiting after death
                     pygame.draw.rect(screen, COLOR_BLUE, player_1_waiting)
-                    pygame.draw.rect(screen, COLOR_WHITE, ball)
                     screen.blit(score_text, score_text_rect)
                     screen.blit(player_lifes_text, player_lifes_rect)
 
